5a6f24881aa9b7e2502650c1ad55da4b5d397a9e
ayman <aymanehab2002@gmail.com>
2025-02-19 16:39:00 +0200

    Partisiong Operators Q03

--- Assignment03/Program.cs
+++ Assignment03/Program.cs
@@ -15,10 +15,19 @@ internal class Program
         #endregion
 
         #region Q02
-        var result = CustomersList.Where(c => c.Region == "WA")
-            .SelectMany(c => c.Orders).Skip(2);
-        foreach (var item in result)
-            Console.WriteLine(item);
+        //var result = CustomersList.Where(c => c.Region == "WA")
+        //    .SelectMany(c => c.Orders).Skip(2);
+        //foreach (var item in result)
+        //    Console.WriteLine(item);
+        #endregion
+
+        #region Q03
+        //int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+        //var result = numbers.TakeWhile((n, i) => n > i);
+        //foreach (int n in result)
+        //{
+        //    Console.WriteLine($"{n} ");
+        //}
         #endregion
 
         #endregion
