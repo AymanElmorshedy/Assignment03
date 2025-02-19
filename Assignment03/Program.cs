f5a23207b169cadd223a5b06a98e2ad8726139f3
ayman <aymanehab2002@gmail.com>
2025-02-19 17:09:38 +0200

    Quantifiers Operators Q02

--- Assignment03/Program.cs
+++ Assignment03/Program.cs
@@ -1,4 +1,5 @@
-﻿using static Assignment03.ListGenerator;
+﻿using System.ComponentModel;
+using static Assignment03.ListGenerator;
 
 namespace Assignment03;
 
@@ -40,15 +41,37 @@ internal class Program
         #endregion
 
         #region Q05
-        int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
-        
-        var result = numbers.SkipWhile((n, i) => n > i);
-        foreach (int n in result)
-        {
-            Console.Write($"{n} ");
-        }
+        //int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+        //var result = numbers.SkipWhile((n, i) => n > i);
+        //foreach (int n in result)
+        //{
+        //    Console.Write($"{n} ");
+        //}
         #endregion
 
+        #endregion
+
+        #region Quantifiers Operators
+        #region Q01
+
+        #endregion
+
+        #region Q02
+        var result = ProductsList.Where(p => p.UnitsInStock == 0).GroupBy(p => p.Category).Select(g => new
+        {
+            Category = g.Key,
+            Products = g.ToList()
+        });
+        foreach (var category in result)
+        {
+            Console.WriteLine($"Category: {category.Category}");
+            foreach (var product in category.Products)
+            {
+                Console.WriteLine($"   - {product.ProductName} ");
+            }
+        }
+        #endregion
         #endregion
         Console.ReadKey();
     }
