95cfaeb16a0bc689c6687962109e5d51b3be3190
ayman <aymanehab2002@gmail.com>
2025-02-19 16:24:52 +0200

    Add project files.

---
+++ Assignment03/Program.cs
@@ -0,0 +1,22 @@
+﻿using static Assignment03.ListGenerator;
+
+namespace Assignment03;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        #region Partisiong Operators
+        #region Q01
+        var result = CustomersList.Where(c=>c.Region== "WA")
+            .SelectMany(c=>c.Orders).Take(3);
+        foreach(var item in result)
+            Console.WriteLine(item);
+        #endregion
+
+        #endregion
+        Console.ReadKey();
+    }
+
+}
+
